baaa1ca11e6c78cc521ac1b568bea3df02cd0a36
shaspen <shaspen@users.noreply.github.com>
2022-11-29 13:27:02 +0330

    bugfix in config_interfaces function

--- python-scripts/01-cisco-config-L3-interfaces/netmiko-config-L3-interfaces.py
+++ python-scripts/01-cisco-config-L3-interfaces/netmiko-config-L3-interfaces.py
@@ -16,35 +16,44 @@ from yaml import safe_load
 import os
 
 # load interface configuration from config.yml file
+
+
 def load_config():
     with open("config.yml", 'r') as file:
         config = safe_load(file)
         return config['interface_configuration']
 
 # load device IPs from config.yml file
+
+
 def load_devices():
     with open("config.yml", 'r') as file:
         config = safe_load(file)
         return config['device_list']
 
 # backup running configuration to file
+
+
 def backup_config(device):
     conn_handler = {
         'device_type': 'cisco_ios',
-        'ip': device ,
-        'username': username ,
+        'ip': device,
+        'username': username,
         'password': password
     }
     net_connect = ConnectHandler(**conn_handler)
     folder = "config_backup_files"
     if not os.path.isdir(folder):
         os.makedirs(folder)
-    filename = "{}-{}-backup.config".format(datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), conn_handler['ip'])
-    with open(os.path.join(folder,filename),'w') as file:
+    filename = "{}-{}-backup.config".format(
+        datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), conn_handler['ip'])
+    with open(os.path.join(folder, filename), 'w') as file:
         backup = net_connect.send_command("show running-config")
         file.write(backup)
 
 # return the output of command <show ip interface brief> as string
+
+
 def write_startup_config(device):
     conn_handler = {
         'device_type': 'cisco_ios',
@@ -57,6 +66,8 @@ def write_startup_config(device):
     print(command)
 
 # return the output of command <show ip interface brief> as string
+
+
 def show_interfaces(device):
     conn_handler = {
         'device_type': 'cisco_ios',
@@ -68,6 +79,8 @@ def show_interfaces(device):
     return net_connect.send_command('show ip interface brief')
 
 # return the list of interfaces with an IP address set on them
+
+
 def l3_interfaces_list(interfaces):
     interface_list = []
     for line in (interfaces.splitlines()):
@@ -78,6 +91,8 @@ def l3_interfaces_list(interfaces):
 
 # configure the interface configuration loaded form config.yml file on each device
 # before deploying any config it make a backup file via backup_config function
+
+
 def config_interfaces(device, interface_list):
     conn_handler = {
         'device_type': 'cisco_ios',
@@ -89,13 +104,15 @@ def config_interfaces(device, interface_list):
     backup_config(device)
     configs = load_config()
     for interface in interface_list:
-        configs.insert(0,"interface {}".format(interface))
-        command = net_connect.send_config_set(configs)
+        interface_fullname = ''
+        interface_fullname = "interface {}".format(interface)
+        command = net_connect.send_config_set([interface_fullname] + configs)
         print(command)
 
-#MAIN function            
-if __name__=="__main__":
-    
+
+# MAIN function
+if __name__ == "__main__":
+
     notice = """    ##############################################################################################################################
     #                                                                                                                            #
     # NOTICE: You are changing the configration on Cisco devices based on configuratoni and devices declarted in config.yml file #
@@ -107,13 +124,14 @@ if __name__=="__main__":
     username = input("please enter the username for devices: ").strip()
     password = input("Please enter password for devices: ").strip()
     devices = load_devices()
-    
+
     for device_ip in devices:
         interfaces = show_interfaces(device_ip)
         l3_interfaces = l3_interfaces_list(interfaces)
         config_interfaces(device_ip, l3_interfaces)
 
-    save_prompt = input("Are you sure to write configuration on Start-up conifuration? [y/n] (default=no) ").strip()
+    save_prompt = input(
+        "Are you sure to write configuration on Start-up conifuration? [y/n] (default=no) ").strip()
     if save_prompt[0] == 'y' or save_prompt[0] == 'Y':
         for device in devices:
             write_startup_config(device)
